62486a67c09a87d29f57eb9af7e91941e278d544
a00126 <87116510+a00126@users.noreply.github.com>
2021-07-10 21:02:46 +0800

    Update jd_cfd.js

--- jd_cfd.js
+++ jd_cfd.js
@@ -58,9 +58,9 @@ cron "5 8,13,19 * * *" script-path=https://gitee.com/lxk0301/jd_scripts/raw/mast
     }
     $.CryptoJS = $.isNode() ? require('crypto-js') : CryptoJS;
     await requestAlgo();
-    let res = {}, res2 = await getAuthorShareCode("https://raw.githubusercontent.com/zero205/updateTeam/main/shareCodes/cfd.json")
-    res = await getAuthorShareCode('https://ghproxy.com/https://raw.githubusercontent.com/zero205/updateTeam/main/shareCodes/jxcfd.json');
-    if (!res2) res2 = await getAuthorShareCode('https://ghproxy.com/https://raw.githubusercontent.com/zero205/updateTeam/main/shareCodes/cfd.json')
+    let res = {}, res2 = await getAuthorShareCode("")
+    res = await getAuthorShareCode('');
+    if (!res2) res2 = await getAuthorShareCode('')
     $.strMyShareIds = [...(res && res.shareId || []),...(res2 && res2.shareId || [])]
     $.strGroupIds = [...(res && res.strGroupIds || []),...(res2 && res2.strGroupIds || [])]
     for (let i = 0; i < cookiesArr.length; i++) {
